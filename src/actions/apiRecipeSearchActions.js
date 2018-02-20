c3983cd4bab6c809e4c1dd39e7483134838a0c7d
Katie West <katiewest820@gmail.com>
2018-02-20 14:36:11 -0800

    test fix

--- src/actions/apiRecipeSearchActions.js
+++ src/actions/apiRecipeSearchActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export const API_RECIPE_SEARCH = 'API_RECIPE_SEARCH';
-let myStorage = window.localStorage;
+//let myStorage = window.localStorage;
 
 export function apiRecipeSearch(url){
   const request = axios.get(url) //, {headers: {authorization: myStorage.authToken}});
